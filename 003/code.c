4c051a2dba6e5f9db0e30ed08dd578ca2bff6db2
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-12-02 16:37:56 +0100

    003

---
+++ 003/code.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define bool int
+#define true 1
+#define false 0
+
+#define ulong unsigned long
+
+ulong largestPrime(ulong num) {
+	ulong largest = 0;
+
+	while (true) {
+		bool found = false;
+		for (ulong i = 2; i*i < num; i++) {
+			if (num % i == 0) {
+				printf("%lu: %lu\n", i, num);
+				if (i > largest) largest = i;
+				num = num / i;
+				found = true;
+				break;
+			}
+		}
+		if (!found) break;
+	}
+	if (num > largest) largest = num;
+
+	return largest;
+}
+
+void main() {
+	printf("%lu\n", largestPrime(600851475143));
+}
